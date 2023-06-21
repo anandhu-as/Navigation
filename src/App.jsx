1621e08baa019320ff7d4e673a0dee71c2a86392
anandhu-as <anandhuas010203@gmail.com>
2023-06-21 14:20:33 +0530

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import Button from "./Button";
+import Button from "./Components/Button";
 const App = () => {
+  
   return (
     <div>
       <div>
-        <Button success rounded >
-          helloo
+        <Button onClick={() => console.log("clicked")} secondary>
+          click me
         </Button>
       </div>
       <div>
-        <Button secondary >
-          Buy now   
-        </Button>
+        <Button primary>Buy now</Button>
       </div>
       <div>
         <Button warning>Click here</Button>
       </div>
       <div>
-        <Button primary outline> submit</Button>
+        <Button primary outline>
+          submit
+        </Button>
       </div>
     </div>
   );
 };
-
 export default App;
---
+++ src/Pages/ButtonPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "../Components/Button";
+const ButtonPage = () => {
+  return (
+    <div>
+      <div>
+        <Button onClick={() => console.log("clicked")} secondary>
+          click me
+        </Button>
+      </div>
+      <div>
+        <Button primary>Buy now</Button>
+      </div>
+      <div>
+        <Button warning>Click here</Button>
+      </div>
+      <div>
+        <Button primary outline>
+          submit
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default ButtonPage;
